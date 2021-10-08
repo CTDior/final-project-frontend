94d93879807e73955fd211a8bc16e0058bbb7062
Emilie Tole <eetole@gmail.com>
2021-10-08 14:35:45 -0400

    services, frontend models

---
+++ src/services/FinalProjectApiServices.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { Group, GroupMember } from "../models/IceBreaker";
+
+const baseUrl: string = process.env.REACT_APP_FINALPROJECT_API_URL || "";
+if (!baseUrl) {
+  console.error("Missing config: REACT_APP_FINALPROJECT_API_URL");
+}
+
+export function fetchAllGroupMembers(): Promise<GroupMember[]> {
+  return axios.get(`${baseUrl}/group-members`).then((res) => res.data);
+}
+
+export function fetchAllGroups(): Promise<Group[]> {
+  return axios.get(`${baseUrl}/groups`).then((res) => res.data);
+}
+
+export function addGroupMember(member: GroupMember): Promise<GroupMember> {
+  return axios.post(`${baseUrl}/group-members`, member).then((res) => res.data);
+}
