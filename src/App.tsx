e9ac5cc5d8648c6fd7bfea40d1b6d1cff53ae3ef
matthew6 <m.holland793@gmail.com>
2021-10-08 13:50:34 -0400

    homepage started

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import React, { useContext } from "react";
 import "./App.css";
 import { signInWithGoogle, signOut } from "./firebaseConfig";
 import { AuthContext } from "./context/AuthContext";
+import Homepage from "./components/Homepage";
 
 function App() {
-  const { user } = useContext(AuthContext);
   return (
     <div className="App">
-      <div>
-        <p>Welcome {user?.displayName}</p>
-        <button onClick={signInWithGoogle}>Sign In With Google</button>
-        <button onClick={signOut}>Sign Out</button>
-      </div>
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { signInWithGoogle, signOut } from "../firebaseConfig";
+import "./Homepage.css";
+
+function Homepage() {
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="Homepage">
+      <div>
+        <p>Welcome {user?.displayName}</p>
+        <button onClick={signInWithGoogle}>Sign In With Google</button>
+        <button onClick={signOut}>Sign Out</button>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
