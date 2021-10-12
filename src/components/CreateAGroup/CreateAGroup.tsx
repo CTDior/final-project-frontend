70803c19cbcea03a6f3096842a372963d90f8c2f
matthew6 <m.holland793@gmail.com>
2021-10-12 13:03:11 -0400

    hardcoded question array added and select fields for admid to select questions, questions added to database with group

--- src/components/CreateAGroup/CreateAGroup.tsx
+++ src/components/CreateAGroup/CreateAGroup.tsx
@@ -12,12 +12,17 @@ import React, { useContext, useState } from "react";
 import { useHistory } from "react-router";
 import { AuthContext, AuthContextProvider } from "../../context/AuthContext";
 import { Group } from "../../models/IceBreaker";
+import questions from "../../questions/questions";
 import { addGroup } from "../../services/FinalProjectApiServices";
 import "./CreateAGroup.css";
 
 function CreateAGroup() {
   const [name, setName] = useState("");
   const [question1, setQuestion1] = useState("");
+  const [question2, setQuestion2] = useState("");
+  const [question3, setQuestion3] = useState("");
+  const [question4, setQuestion4] = useState("");
+  const [question5, setQuestion5] = useState("");
   const { user } = useContext(AuthContext);
   function handleAddGroup(group: Group) {
     addGroup(group).then((newGroup) => {
@@ -36,7 +41,7 @@ function CreateAGroup() {
     const group: Group = {
       name: name,
       adminUid: user?.uid!,
-      profileQuestions: [question1],
+      profileQuestions: [question1, question2, question3, question4, question4],
     };
     handleAddGroup(group);
     setName("");
@@ -46,6 +51,7 @@ function CreateAGroup() {
     <div className="CreateAGroup">
       <FormControl>
         <TextField
+          required
           autoFocus
           margin="dense"
           id="PostForm__title"
@@ -58,13 +64,74 @@ function CreateAGroup() {
         ></TextField>
         <InputLabel id="demo-simple-select-label"></InputLabel>
         <Select
+          required
           labelId="questions"
           id="question1"
           value={question1}
-          label="Age"
+          label="Question 1"
           onChange={(e) => setQuestion1(e.target.value)}
         >
-          <MenuItem value={"A"}>"Do you fold your pizza?"</MenuItem>
+          {questions.map((question) => (
+            <MenuItem key={question._id} value={question._id}>
+              {question.text}
+            </MenuItem>
+          ))}
+        </Select>
+        <Select
+          required
+          labelId="questions"
+          id="question2"
+          value={question2}
+          label="Question 2"
+          onChange={(e) => setQuestion2(e.target.value)}
+        >
+          {questions.map((question) => (
+            <MenuItem key={question._id} value={question._id}>
+              {question.text}
+            </MenuItem>
+          ))}
+        </Select>
+        <Select
+          required
+          labelId="questions"
+          id="question3"
+          value={question3}
+          label="Question 3"
+          onChange={(e) => setQuestion3(e.target.value)}
+        >
+          {questions.map((question) => (
+            <MenuItem key={question._id} value={question._id}>
+              {question.text}
+            </MenuItem>
+          ))}
+        </Select>
+        <Select
+          required
+          labelId="questions"
+          id="question4"
+          value={question4}
+          label="Question 4"
+          onChange={(e) => setQuestion4(e.target.value)}
+        >
+          {questions.map((question) => (
+            <MenuItem key={question._id} value={question._id}>
+              {question.text}
+            </MenuItem>
+          ))}
+        </Select>
+        <Select
+          required
+          labelId="questions"
+          id="question5"
+          value={question5}
+          label="Question 5"
+          onChange={(e) => setQuestion5(e.target.value)}
+        >
+          {questions.map((question) => (
+            <MenuItem key={question._id} value={question._id}>
+              {question.text}
+            </MenuItem>
+          ))}
         </Select>
         <Button onClick={handleSubmit}>Create Group</Button>
       </FormControl>
--- src/questions/questions.ts
+++ src/questions/questions.ts
@@ -1,4 +1,4 @@
-export const questions = [
+const questions = [
   {
     _id: "A",
     text: "Do you fold your pizza?",
@@ -14,4 +14,81 @@ export const questions = [
     text: "Would you rather be reincarnated as a cat or a dog?",
     options: ["Cat", "Dog", "Neither"],
   },
+  {
+    _id: "D",
+    text: "Would you rather be reincarnated as a cat or a dog?",
+    options: ["Cat", "Dog", "Neither"],
+  },
+  {
+    _id: "E",
+    text: "What is an item on your bucket list?",
+    options: ["Skydive", "Bungee Jump", "Travel to all the continents"],
+  },
+  {
+    _id: "F",
+    text: "What is your favorite part of working from home?",
+    options: ["No Commute", "Pajama Pants", "I'd rather be in the office"],
+  },
+  {
+    _id: "G",
+    text: "What is your favorite movie genre to watch?",
+    options: ["Action", "Comedy", "Drama", "Horror", "None of these"],
+  },
+  {
+    _id: "H",
+    text: "What is your favorite thing to eat for breakfast?",
+    options: ["Eggs", "Cereal", "Leftover Pizza"],
+  },
+  {
+    _id: "I",
+    text: "When are you most productive?",
+    options: ["Morning", "Afternoon", "Evening", "Night"],
+  },
+  {
+    _id: "J",
+    text: "What is your favorite season?",
+    options: ["Spring", "Summer", "Winter", "Fall"],
+  },
+  {
+    _id: "K",
+    text: "What would it take to give up your smartphone for a month?",
+    options: [
+      "I would gladly do it",
+      "Not even a million dollars",
+      "Something else",
+    ],
+  },
+  {
+    _id: "L",
+    text: "Would you rather have a pet sloth or a pet parrot?",
+    options: ["Parrot", "Sloth", "Neither", "Something else"],
+  },
+  {
+    _id: "M",
+    text: "There is a free, round-trip shuttle to Mars. The catch: it will take one year of your life to go, visit, and come back. Are you in?",
+    options: ["Yes", "No", "Maybe"],
+  },
+  {
+    _id: "N",
+    text: "Which do you prefer most?",
+    options: ["Cookies", "Brownies", "Cake", "Ice Cream"],
+  },
+  {
+    _id: "O",
+    text: "Favorite Festival Food",
+    options: [
+      "Elephant Ear",
+      "Corndog",
+      "Cotton Candy",
+      "Something else",
+      "I don't eat any of that",
+    ],
+  },
+  {
+    _id: "P",
+    text: "Would it be better to start work five years later or end five years earlier?",
+    options: ["Start Later", "End Earlier", "Neither", "Something else"],
+  },
 ];
+
+export default questions;
