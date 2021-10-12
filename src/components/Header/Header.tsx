dc57cd9ba175cd76a874407f3411cf28d3b62686
CTDior <83152946+CTDior@users.noreply.github.com>
2021-10-12 13:19:27 -0400

    added ability to go home with title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
         <Toolbar>
           {user && <TemporaryDrawer />}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            App Name
+            <a href="/">App Name</a>
           </Typography>
           {user ? (
             <div className="signed-in">
