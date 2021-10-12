8f7b50ec69fc2ef2344f85597efc584620e4c9fa
Emilie Tole <eetole@gmail.com>
2021-10-12 11:46:04 -0400

    disabled site until user signs in

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,33 @@ import { addGroup } from "./services/FinalProjectApiServices";
 import { Group } from "./models/IceBreaker";
 import GroupPage from "./components/GroupPage/GroupPage";
 import Header from "./components/Header/Header";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="App">
       <Router>
         <Header />
-        <Switch>
-          <Route path="/" exact>
-            <Homepage />
-          </Route>
-          <Route path="/group/create" exact>
-            <CreateAGroup />
-          </Route>
-          <Route path="/group/:id">
-            <GroupPage />
-          </Route>
-        </Switch>
+        {user ? (
+          <Switch>
+            <Route path="/" exact>
+              <Homepage />
+            </Route>
+            <Route path="/group/create" exact>
+              <CreateAGroup />
+            </Route>
+            <Route path="/group/:id">
+              <GroupPage />
+            </Route>
+          </Switch>
+        ) : (
+          <div className="login">
+            <p>Please Login To Continue</p>
+          </div>
+        )}
       </Router>
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <TemporaryDrawer />
+          {user && <TemporaryDrawer />}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             App Name
           </Typography>
