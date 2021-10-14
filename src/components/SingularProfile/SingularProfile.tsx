38cc9831a0804936cb3c74d0412bdf0d2d93c9a2
matthew6 <m.holland793@gmail.com>
2021-10-13 21:40:22 -0400

    group profiles almost displaying questions and answers

--- src/components/SingularProfile/SingularProfile.tsx
+++ src/components/SingularProfile/SingularProfile.tsx
@@ -1,13 +1,28 @@
 import { GroupMember } from "../../models/IceBreaker";
 import "./SingularProfile.css";
 import questions from "../../questions/questions";
-import { Card, Typography } from "@mui/material";
+import { Card, containerClasses, Typography } from "@mui/material";
+import { group } from "console";
 
 interface Props {
   groupMember: GroupMember;
 }
 
 const SingularProfile = ({ groupMember }: Props) => {
+  console.log(groupMember);
+
+  // const memberAnswers = groupMember.answers;
+  // console.log(memberAnswers);
+  const questionsTextAndAnswers = groupMember.answers.map((item) => {
+    const container: any = {};
+    container.question = questions.find(
+      (eachQuestion) => eachQuestion._id === item.questionId
+    );
+    container.answer = item.answer;
+    return container;
+  });
+  console.log(questionsTextAndAnswers);
+
   return (
     <div className="SingularProfile">
       <Card variant="outlined">
@@ -20,14 +35,21 @@ const SingularProfile = ({ groupMember }: Props) => {
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
           {groupMember.favoriteColor}
         </Typography>
-        <Typography variant="body2">
+        <Typography>
+          {questionsTextAndAnswers.map(
+            (eachQA) =>
+              //grab question ids from each answer and find each id of question and where it matches in order to display
+              eachQA.answer
+          )}
+        </Typography>
+        {/* <Typography variant="body2">
           {groupMember.answers.map(
             (eachAnswer) =>
               //grab question ids from each answer and find each id of question and where it matches in order to display
               eachAnswer.answer
           )}
           <br />
-        </Typography>
+        </Typography> */}
       </Card>
     </div>
   );
