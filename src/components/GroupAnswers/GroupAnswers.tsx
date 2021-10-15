80949670b45bf41d8f118684d7360a4fccbd9e60
Emilie Tole <eetole@gmail.com>
2021-10-15 16:06:35 -0400

    answerArray, answerCount in GroupAnswers

--- src/components/GroupAnswers/GroupAnswers.tsx
+++ src/components/GroupAnswers/GroupAnswers.tsx
@@ -10,20 +10,30 @@ interface Props {
 
 const GroupAnswers = ({ groupMembers, group }: Props) => {
   console.log(groupMembers);
-  const questionText = group.profileQuestions.map((item) => {
+  const questionArray = group.profileQuestions.map((item) => {
     const container: any = {};
     container.question = questions.find(
       (eachQuestion) => eachQuestion._id === item
     );
-
     return container;
   });
 
+  const answerArray = questionArray.map((options) => {
+    return options.question.options;
+  });
+
+  const answerCount = answerArray.map((item) => {
+    const container: any = {};
+    container.item = item;
+    container.count = 0;
+    return container;
+  });
+  console.log(answerCount);
   return (
     <div className="GroupAnswers">
       {group.profileQuestions}
       <ol className="GroupAnswers__QuestionList">
-        {questionText.map((eachQuestion) => (
+        {questionArray.map((eachQuestion) => (
           <li key={eachQuestion.question._id}>
             {console.log(eachQuestion)}
             {eachQuestion.question.text}
