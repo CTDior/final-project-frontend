1f98fc17e3cf330478ea6dad8cb802f3be809274
CTDior <83152946+CTDior@users.noreply.github.com>
2021-10-18 16:57:42 -0400

    added tab buttons

--- src/components/GroupPage/GroupPage.tsx
+++ src/components/GroupPage/GroupPage.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { responsiveFontSizes } from "@mui/material";
+import { Box, responsiveFontSizes, Tab, Tabs, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Group, GroupMember } from "../../models/IceBreaker";
@@ -16,9 +16,42 @@ import "./GroupPage.css";
 import questions from "../../questions/questions";
 
 import GroupAnswers from "../GroupAnswers/GroupAnswers";
+import React from "react";
 
 interface RouteParams {
   id: string;
+}
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
 const GroupPage = () => {
@@ -59,6 +92,11 @@ const GroupPage = () => {
       return user?.uid === groupMember.userUid;
     }
   );
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   if (currentUserInGroup && group._id) {
     return (
@@ -74,11 +112,27 @@ const GroupPage = () => {
             </li>
           ))}
         </ol>
-        <MemberProfilesList
-          groupMembers={groupMembers}
-          currentUserInGroup={currentUserInGroup}
-        />{" "}
-        <GroupAnswers groupMembers={groupMembers} group={group} />
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              <Tab label="Members" {...a11yProps(0)} />
+              <Tab label="Answers" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <MemberProfilesList
+              groupMembers={groupMembers}
+              currentUserInGroup={currentUserInGroup}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <GroupAnswers groupMembers={groupMembers} group={group} />
+          </TabPanel>
+        </Box>{" "}
       </div>
     );
   } else {
