dbdf9a3ab5ed8a00231b3886a74cecc4b1f0ea4d
matthew6 <m.holland793@gmail.com>
2021-10-18 14:32:32 -0400

    answer graph bars showing up

--- src/components/GroupAnswers/SingularQuestion.tsx
+++ src/components/GroupAnswers/SingularQuestion.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import { Card } from "@mui/material";
 import { Group, GroupMember, Question } from "../../models/IceBreaker";
 import questions from "../../questions/questions";
 import "./SingularQuestion.css";
@@ -16,8 +17,11 @@ const SingularQuestion = ({ groupMembers, questionId }: Props) => {
   if (!question) {
     return <div>Error: No such question Id.</div>;
   }
+
+  let answerBarWidth;
   const countArray = question.options.map((eachAnswer) => {
     let counter = 0;
+
     for (const groupMember of groupMembers) {
       for (const groupMemberAnswer of groupMember.answers) {
         if (
@@ -29,19 +33,44 @@ const SingularQuestion = ({ groupMembers, questionId }: Props) => {
       }
     }
 
-    return { name: eachAnswer, count: counter };
+    return {
+      name: eachAnswer,
+      count: counter,
+      answerBarWidth: Number(
+        ((counter / groupMembers.length) * 100).toFixed(1)
+      ),
+    };
   });
+
+  // //bar width?
+  // let answerBarWidth = (strawberryVotes/totalVotes * 100).toFixed(1)
+  // let eachAnswerStyles = {
+  //   width: answerBarWidth + "%",
+  //   height: 20 + "px",
+  //   backgroundColor: "#fc5a8d",
+  // };
   console.log(countArray);
 
   return (
-    <div className="SingularQuestion">
+    <Card className="SingularQuestion" variant="outlined">
       {question.text}
       {countArray.map((eachAnswer) => (
-        <li>
-          {eachAnswer.name} - {eachAnswer.count}
-        </li>
+        <ul style={{ listStyle: "none" }}>
+          <li>
+            {eachAnswer.name}:{" "}
+            {((eachAnswer.count / groupMembers.length) * 100).toFixed(1)}%
+          </li>
+          <div
+            className="eachAnswerStyles"
+            style={{
+              backgroundColor: "#2196F3",
+              height: "20px",
+              width: eachAnswer.answerBarWidth + "%",
+            }}
+          ></div>
+        </ul>
       ))}
-    </div>
+    </Card>
   );
 };
 
